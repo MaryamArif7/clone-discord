3b68b013d743788f092c110f2784c2c700c05293
MaryamArif7 <www.maryamarif24242@gmail.com>
2023-09-11 12:40:30 +0500

    remove the error

--- clone-discord/lib/initial.profile.ts
+++ clone-discord/lib/initial.profile.ts
@@ -12,13 +12,13 @@ export const intialProfile=async()=>{
 userId:user.id
         }
     });
-    if(!profile){
+    if(profile){
         return profile;
     }
     const newProfile=await db.profile.create({
         data:{
             userId:user.id,
-            name:'$(user.firstName) $(user.lastName)',
+            name: `${user.firstName} ${user.lastName}`,
             imageUrl:user.imageUrl,
             email:user.emailAddresses[0].emailAddress
         }
