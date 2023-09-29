fba862b671063e15adc52b6e1d5f608aa4bbe810
MaryamArif7 <www.maryamarif24242@gmail.com>
2023-09-29 19:13:27 +0500

    errors

--- clone-discord/lib/current-profile.ts
+++ clone-discord/lib/current-profile.ts
@@ -1,10 +1,14 @@
 import { auth } from "@clerk/nextjs";
-
+// we are checking that if user is already logged in 
 import { db } from "@/lib/db";
 
 export const currentProfile = async () => {
   const { userId } = auth();
+/*
+ auth fun from clerk is called nd it reurns an object whcih contains information about the
+about eh currentlu authenticted user then object is destructed to get the information of user id
 
+ */
   if (!userId) {
     return null;
   }
