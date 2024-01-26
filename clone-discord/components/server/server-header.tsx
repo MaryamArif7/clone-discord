f9f0a556959fecfb85205688bd06a647eadfbac8
MaryamArif7 <www.maryamarif24242@gmail.com>
2024-01-26 12:09:36 +0500

    13.1

--- clone-discord/components/server/server-header.tsx
+++ clone-discord/components/server/server-header.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole} from "@prisma/client";
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+import { DropdownMenu, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
 import { DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 interface ServerHeaderProps{
  //11.3:serversidebar:
   /*   cant use the prisma server beacuse this server also has memebers channels
@@ -17,34 +18,87 @@ export const ServerHeader =({
     server,
     role 
 }:ServerHeaderProps)=>{
+  //12.3 after adding types in use modal store for the onopen 
+  const { onOpen}=useModal();
     const isAdmin=role===MemberRole.ADMIN;
     const isModerator=isAdmin||role=== MemberRole.MODERATOR;
     return (
-        <DropdownMenu>
-        <DropdownMenuTrigger
-          className="focus:outline-none" 
-          asChild
+      //12.1 added the  Dropdownmenu items
+      <DropdownMenu>
+      <DropdownMenuTrigger
+        className="focus:outline-none" 
+        asChild
+      >
+        <button
+          className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
         >
-          <button
-            className="w-full text-md font-semibold
-             px-3 flex items-center h-12
-              border-neutral-200 dark:border-neutral-800
-               border-b-2 hover:bg-zinc-700/10
-                dark:hover:bg-zinc-700/50 transition"
-          >
-            {server.name}
-            <ChevronDown className="h-5 w-5 ml-auto" />
-          </button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent 
-        className="w-56 text-xs font-medium text-black
-        dark:text-neutral-400 space-y-[2px]">    
+          {server.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+      >
+      {/*12.4 opening the modal for invite when clicks on the onOpen */}
         {isModerator && (
-            <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400
-            px-3 py-2 text-sm cursor-pointer" >
-                Invite people
-            </DropdownMenuItem>
-        )}        
+
+          <DropdownMenuItem
+          onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
+         {isAdmin && (
+          <DropdownMenuItem
+      
+            className=" px-3 py-2 text-sm cursor-pointer"
+          >
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
+         {isAdmin && (
+          <DropdownMenuItem
+      
+            className=" px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Members
+            <Users className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
+         {isModerator && (
+          <DropdownMenuItem
+      
+            className=" px-3 py-2 text-sm cursor-pointer"
+          >
+            Create Channel 
+            <PlusCircle className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
+         {isModerator && (
+          <DropdownMenuSeparator />
+         )}
+  {isAdmin && (
+          <DropdownMenuItem
+      
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
+                
+  {!isAdmin && (
+          <DropdownMenuItem
+      
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Leave the Server
+            <LogOut className="h-4 w-4 ml-auto " />
+          </DropdownMenuItem>
+        )}
         </DropdownMenuContent>
         </DropdownMenu>
     )
--- clone-discord/hooks/use-modal-store.ts
+++ clone-discord/hooks/use-modal-store.ts
@@ -1,8 +1,8 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 //10:modalstore
+//12.2 to invite people when they click on the invite peopple
 export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
-
 interface ModalData {
   server?: Server;
   channel?: Channel;
@@ -26,3 +26,8 @@ export const useModal = create<ModalStore>((set) => ({
   onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false })
 }));
+/* defining the potentiol items we can send in the modal not 
+only  setting the type but also data ,onopen has a type and dat
+if we pass in the data  it will be store in the modal store like gonna open  the invite model
+with some infirmation about  that server , we are going to send the server
+inside the  data using  the onopen function*/
