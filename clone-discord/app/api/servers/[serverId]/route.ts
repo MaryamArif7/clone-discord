c3e5aa4b3aba7d372885da7d15d0ab5f95efc96a
MaryamArif7 <www.maryamarif24242@gmail.com>
2024-02-12 11:09:40 +0500

    21

--- clone-discord/app/api/servers/[serverId]/route.ts
+++ clone-discord/app/api/servers/[serverId]/route.ts
@@ -1,57 +1,63 @@
+
+//18... delete the server :  potting the route for the delete the server 
+
+import { NextResponse } from "next/server";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-//18... delete the server :  potting the route for the delete the server 
+
 export async function DELETE(
-    req:Request,
-    {params}:{params:{serverId:string}}
-){
-    try{
-       const profile=await currentProfile();
-       const {name ,imageUrl}=await req.json();
-       if(!profile){
-        return new NextResponse("unauthorized access is not allowed",{status:401});
-       }
-       const server=await db.server.delete({
-        where:{
-            id:params.serverId,
-            profileId:profile.id,
-        }
-      
-       });
-     return NextResponse.json(server)
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-    catch(error){
-        console.log("[SERVER_ID_DELETE]",error);
-        return new NextResponse("internal server error",{status:500});
-    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
 //14.6 edit: the route file for patch
-export async function PATCH(
-    req:Request,
-    {params}:{params:{serverId:string}}
-){
-    try{
-       const profile=await currentProfile();
-       const {name ,imageUrl}=await req.json();
-       if(!profile){
-        return new NextResponse("unauthorized access is not allowed",{status:401});
-       }
-       const server=await db.server.update({
-        where:{
-            id:params.serverId,
-            profileId:profile.id,
-        },
-        data:{
-         name,
-         imageUrl,
 
-        }
-       })
-     return NextResponse.json(server)
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-    catch(error){
-        console.log("[SERVER_ID_PATCH]",error);
-        return new NextResponse("internal server error",{status:500});
-    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
